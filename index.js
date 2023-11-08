64c4fb28432f2439425ccea9fd8af183292f2cd9
Roman Horobets <horobetsroman@prahs.com>
2023-11-08 22:00:27 +0200

    chore: :rocket: init

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const pug = require("pug");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
