6e69208fdb6249a00955e7ad3d3d3b7bc2730d39
Roman Horobets <horobetsroman@prahs.com>
2023-11-09 21:47:18 +0200

    chore: :zap: add compression and cssnano

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const pug = require("pug");
+const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(compression());
 
 app.get("/", (req, res) => {
   res.render("index");
